7e32c93372cbb86563090723baf8032670f4f38c
Oleksandr Tereshchenko <toporok@gmail.com>
2025-03-30 14:36:19 +0300

    try BrightData

---
+++ try_brightdata_google_search.py
@@ -0,0 +1,15 @@
+import asyncio
+import brightdata
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+async def main():
+    # try BrightData Google Search
+    ...
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
