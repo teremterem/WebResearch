be3a02cd32a216421ccb597b18b5c0e282d135f9
Oleksandr Tereshchenko <toporok@gmail.com>
2025-03-31 22:47:51 +0300

    openai structured response

--- web_research.py
+++ web_research.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pprint import pprint
 
 from dotenv import load_dotenv
@@ -15,33 +16,46 @@ openai_agent = OpenAIAgent.fork(
 )
 
 
-class Step(BaseModel):
-    explanation: str
-    output: str
+class WebSearch(BaseModel):
+    rationale: str
+    web_search_query: str
 
 
-class MathReasoning(BaseModel):
-    steps: tuple[Step, ...]
-    final_answer: str
+class WebSearchesToBeDone(BaseModel):
+    web_searches: tuple[WebSearch, ...]
 
 
 @miniagent
 async def research_agent(ctx: InteractionContext) -> None:
     # response_promises = openai_agent.trigger(ctx.message_promises)
     # ctx.reply(response_promises)
-    messages = await aprepare_dicts_for_openai(ctx.message_promises)
-    pprint(messages)
+    messages = await aprepare_dicts_for_openai(
+        ctx.message_promises,
+        system=(
+            "Your job is to breakdown the user's question into a list of web searches that need to be done to answer "
+            "the question. Current date is " + datetime.now().strftime("%Y-%m-%d")
+        ),
+    )
     response = await openai_client.beta.chat.completions.parse(
         model="gpt-4o",
         messages=messages,
-        response_format=MathReasoning,
+        response_format=WebSearchesToBeDone,
     )
-    pprint(response)
+    parsed: WebSearchesToBeDone = response.choices[0].message.parsed
+    for web_search in parsed.web_searches:
+        ctx.reply(f"{web_search.rationale}\n> {web_search.web_search_query}")
 
 
 async def main():
-    question = input("Enter a question: ")
+    # question = input("Enter a question: ")
+    question = (
+        "I'm thinking of moving from Lviv to Kyiv — what should I know about the cost of living, neighborhoods, gyms, "
+        "and, most importantly, finding an apartment if I have two cats?"
+    )
+
     response_promises = research_agent.trigger(question)
+
+    print("\nResearching...\n")
     async for message_promise in response_promises:
         async for token in message_promise:
             print(token, end="", flush=True)
